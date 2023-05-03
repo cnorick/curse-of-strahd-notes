8daac22b0e20bf8c497d37d95a00756b8d7acfe8
Nathan Orick <cnathanorick@gmail.com>
2023-05-02 21:38:02 -0400

    vault backup: 2023-05-02 21:38:02

--- Website/scripts/fix-broken-urls.py
+++ Website/scripts/fix-broken-urls.py
@@ -1,15 +1,15 @@
 import os
+import sys
 from bs4 import BeautifulSoup, Doctype
 
-directory = '/home/brian/Code/sof'
-for root, dirnames, filenames in os.walk(directory):
-    for filename in filenames:
-        if filename.endswith('.html'):
-            fname = os.path.join(root, filename)
-            print('Filename: {}'.format(fname))
-            with open(fname) as handle:
-                soup = BeautifulSoup(handle.read(), 'html.parser')
-                for item in soup.contents:
-                    if isinstance(item, Doctype):
-                        print('Doctype: {}'.format(item))
-                        break
+for root, dirnames, filenames in os.walk(sys.argv[1]):
+  for filename in filenames:
+    if filename.endswith('.html'):
+      fname = os.path.join(root, filename)
+      print('Filename: {}'.format(fname))
+      with open(fname) as handle:
+        soup = BeautifulSoup(handle.read(), 'html.parser')
+        for item in soup.contents:
+          if isinstance(item, Doctype):
+            print('Doctype: {}'.format(item))
+            break
