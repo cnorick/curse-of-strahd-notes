ddb67339b2387361aa82b7e0d641c8295b661ace
Nathan Orick <cnathanorick@gmail.com>
2023-05-02 21:31:51 -0400

    vault backup: 2023-05-02 21:31:51

--- Website/scripts/clean-notes.py
+++ Website/scripts/clean-notes.py
@@ -8,11 +8,12 @@ with open(sys.argv[1], 'r+') as my_file:
   
   # Make all non-absolute links relative to the root
   for a in soup.find_all('a'):
-    print(a.href)
+    print(a['href'])
     if(not a['href'].startswith('http')):
       # a['href'] = '/' + a['href']
       # Temp fix for html exporter bug
-      a['href'] = '/' + a['href'].lower().replace('users/nathan/appdata/local/obsidian', '')
+      a['href'] = a['href'].replace('users/nathan/appdata/local/obsidian', '')
+      print(a['href'])
 
   # Hide all the links in the session notes
   sessionNotes = soup.find(id="Session_1").find_parent(class_='block-language-dataviewjs')
