84655353cdba074b8442cf9e1b2f348cc13b35b1
Nathan Orick <cnathanorick@gmail.com>
2023-05-02 22:54:04 -0400

    vault backup: 2023-05-02 22:54:04

--- Website/scripts/publish.sh
+++ Website/scripts/publish.sh
@@ -9,7 +9,7 @@ s3Path="s3://tater-cos-site"
 cloudFrontId="EO6PCDBX74WRI"
 
 ./scripts/clean-notes.sh
-python3 ./scripts/fix-broken-urls.py site
+python3 ./scripts/fix-broken-urls.py site/
 
 #cp 'no-spoilers/Campaign Notes.pdf' no-spoilers/campaign-notes.pdf
 cp -r no-spoilers site/
