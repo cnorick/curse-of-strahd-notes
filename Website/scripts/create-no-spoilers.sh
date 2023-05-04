27c00107187e7142ee88a73a35d05b573fb5e45b
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 14:45:04 -0400

    vault backup: 2023-05-04 14:45:04

--- Website/scripts/create-no-spoilers.sh
+++ Website/scripts/create-no-spoilers.sh
@@ -3,5 +3,5 @@
 
 cp site/campaign-notes.html no-spoilers/campaign-notes/index.html
 cp -r site/styles site/scripts no-spoilers/campaign-notes
-python3 scripts/clean-notes.py no-spoilers/campaign-notes/index.html
+# python3 scripts/clean-notes.py no-spoilers/campaign-notes/index.html
 
