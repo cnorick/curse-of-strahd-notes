1b14feddb43b9cf36d1af91792e2237a9b7d6a35
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 15:34:04 -0400

    vault backup: 2023-05-04 15:34:04

--- Website/scripts/clean-notes.py
+++ Website/scripts/clean-notes.py
@@ -1,26 +1,47 @@
-from bs4 import BeautifulSoup
+import os
 import sys
+from bs4 import BeautifulSoup
 
-cleanContent = ''
-with open(sys.argv[1], 'r+') as my_file:
-  text = my_file.read()
-  soup = BeautifulSoup(text, 'html.parser')
-  
-  # Make all non-absolute links relative to the root
-  for a in soup.find_all('a'):
-    if(not a['href'].startswith('http')):
-      # a['href'] = '/' + a['href']
-      pass
+def overwriteFile(file, soup):
+  content = str(soup)
+  file.seek(0)
+  file.write(content)
+  file.truncate()
 
-  # Hide all the links in the session notes
-  sessionNotes = soup.find(id="Session_1").find_parent(class_='block-language-dataviewjs')
-  for a in sessionNotes.find_all('a', class_='internal-link'):
-    a.string.wrap(soup.new_tag('b'))
-    a.unwrap()
+def updateIndexFile(filename):
+  with open(filename, 'r+') as indexFile:
+    text = indexFile.read()
+    soup = BeautifulSoup(text, 'html.parser')
 
-  cleanContent = str(soup)
+    # Make all non-absolute links relative to the root
+    print('Fixing no-spoilers index file')
+    for a in soup.find_all('a'):
+      if(not a['href'].startswith('http')):
+        a['href'] = '../../' + a['href']
 
-  my_file.seek(0)
-  my_file.write(cleanContent)
-  my_file.truncate()
-  
+    sessionAnchors = soup.find(id="Session_Notes").parent.nextSibling.find_all('a')
+    for a in sessionAnchors:
+      # Make all session note links relative
+      print('Fixing Link to: {}'.format(a.text))
+      a['href'] = a['href'][6:]
+
+    overwriteFile(indexFile, soup)
+
+def updateSessionFiles(folder):
+  for root, dirnames, filenames in os.walk(folder):
+    for filename in filenames:
+      if filename.endswith('.html'):
+        fname = os.path.join(root, filename)
+        print('Fixing URLs for: {}'.format(fname))
+        with open(fname, 'r+') as handle:
+          soup = BeautifulSoup(handle.read(), 'html.parser')
+          for a in soup.find_all('a', class_='internal-link'):
+            a.wrap(soup.new_tag('b'))
+            a.unwrap()
+          overwriteFile(handle, soup)
+
+
+
+
+updateIndexFile(sys.argv[1])
+updateSessionFiles(sys.argv[2])
--- Website/scripts/create-no-spoilers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Creates the no-spoilers part of the site.
-
-cp site/campaign-notes.html no-spoilers/campaign-notes/index.html
-cp -r site/styles site/scripts no-spoilers/campaign-notes
-cp -r site/session-notes no-spoilers/campaign-notes/session-notes
-python3 scripts/create-no-spoilers.py no-spoilers/campaign-notes/index.html no-spoilers/campaign-notes/session-notes
-
