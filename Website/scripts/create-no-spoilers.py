c815fe40b536d46d8a6c7cc941d88bc9318099d5
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 15:03:02 -0400

    vault backup: 2023-05-04 15:03:02

--- Website/scripts/create-no-spoilers.py
+++ Website/scripts/create-no-spoilers.py
@@ -7,8 +7,14 @@ with open(sys.argv[1], 'r+') as indexFile:
   text = indexFile.read()
   soup = BeautifulSoup(text, 'html.parser')
 
+  # Make all non-absolute links relative to the root
+  for a in soup.find_all('a'):
+    if(not a['href'].startswith('http')):
+      a['href'] = '/' + a['href']
+
   sessionAnchors = soup.find(id="Session_Notes").find_all('a')
   for a in sessionAnchors:
-    
+    # Make all session note links relative
 
-shutil.copyfile
+
+# shutil.copyfile
