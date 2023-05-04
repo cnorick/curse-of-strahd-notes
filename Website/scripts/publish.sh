11849686e0e76dde5790a6a29a3cb7d312e25bfc
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 14:07:43 -0400

    vault backup: 2023-05-04 14:07:43

--- Website/scripts/publish.sh
+++ Website/scripts/publish.sh
@@ -11,9 +11,8 @@ cloudFrontId="EO6PCDBX74WRI"
 ./scripts/clean-notes.sh
 # python3 ./scripts/fix-broken-urls.py site/
 
-#cp 'no-spoilers/Campaign Notes.pdf' no-spoilers/campaign-notes.pdf
 cp -r no-spoilers site/
 
-# aws s3 sync site $s3Path $flags
+aws s3 sync site $s3Path $flags
 
 aws cloudfront create-invalidation --distribution-id $cloudFrontId --paths '/*'
