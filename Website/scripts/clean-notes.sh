6f13c7ecfa18b0ebc28b1a5c55bd69930fc8f5fe
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 15:47:05 -0400

    vault backup: 2023-05-04 15:47:05

--- Website/scripts/clean-notes.sh
+++ Website/scripts/clean-notes.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # Cleans links from the session notes
 
-# cp site/campaign-notes.html no-spoilers/campaign-notes/index.html
-# cp -r site/styles site/scripts no-spoilers/campaign-notes
-# cp -r site/session-notes no-spoilers/session-notes
-# python3 scripts/clean-notes.py no-spoilers/campaign-notes/index.html
-
 cp site/campaign-notes.html no-spoilers/campaign-notes/index.html
 cp -r site/styles site/scripts no-spoilers/campaign-notes
 cp -r site/session-notes no-spoilers/campaign-notes/session-notes
