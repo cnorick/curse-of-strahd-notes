a9b2fbb09f2bfee7b1d6a97439391c2ab7a46b35
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 14:53:40 -0400

    vault backup: 2023-05-04 14:53:40

--- Website/scripts/create-no-spoilers.py
+++ Website/scripts/create-no-spoilers.py
@@ -7,6 +7,8 @@ with open(sys.argv[1], 'r+') as indexFile:
   text = indexFile.read()
   soup = BeautifulSoup(text, 'html.parser')
 
-  soup.find(id="Session_Notes")
+  sessionAnchors = soup.find(id="Session_Notes").find_all('a')
+  for a in sessionAnchors:
+    
 
 shutil.copyfile
