ab770b72ea69553cdcfa503f48a0a165e00a6c01
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 14:46:39 -0400

    vault backup: 2023-05-04 14:46:39

---
+++ Website/scripts/create-no-spoilers.py
@@ -0,0 +1,25 @@
+import shutil
+import os
+import sys
+from bs4 import BeautifulSoup
+
+for root, dirnames, filenames in os.walk(sys.argv[1]):
+  for filename in filenames:
+    if filename.endswith('.html'):
+      fname = os.path.join(root, filename)
+      print('Fixing URLs for: {}'.format(fname))
+      with open(fname, 'r+') as handle:
+        soup = BeautifulSoup(handle.read(), 'html.parser')
+        for a in soup.find_all('a'):
+          if(not a['href'].startswith('http')):
+            # Temp fix for html exporter bug
+            a['href'] = a['href'].replace('users/nathan/appdata/local/obsidian', '')
+        
+        cleanContent = str(soup)
+
+        handle.seek(0)
+        handle.write(cleanContent)
+        handle.truncate()
+
+
+shutil.copyfile
