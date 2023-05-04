4d373b226d4cb54cdab6a0e0a2736fd68aca852d
Nathan Orick <cnathanorick@gmail.com>
2023-05-04 15:11:02 -0400

    vault backup: 2023-05-04 15:11:02

--- Website/scripts/create-no-spoilers.py
+++ Website/scripts/create-no-spoilers.py
@@ -3,26 +3,28 @@ import os
 import sys
 from bs4 import BeautifulSoup
 
-with open(sys.argv[1], 'r+') as indexFile:
-  text = indexFile.read()
-  soup = BeautifulSoup(text, 'html.parser')
-
-  # Make all non-absolute links relative to the root
-  for a in soup.find_all('a'):
-    if(not a['href'].startswith('http')):
-      a['href'] = '/' + a['href']
-
-  sessionAnchors = soup.find(id="Session_Notes").find_all('a')
-  for a in sessionAnchors:
-    # Make all session note links relative
-    a['href'] = a['href'][1:]
-
-  cleanContent = str(soup)
-
-
-# shutil.copyfile
-
-def overwriteFile(file, content):
+def overwriteFile(file, soup):
+  content = str(soup)
   file.seek(0)
   file.write(content)
   file.truncate()
+
+def updateIndexFile(filename):
+  with open(filename, 'r+') as indexFile:
+    text = indexFile.read()
+    soup = BeautifulSoup(text, 'html.parser')
+
+    # Make all non-absolute links relative to the root
+    for a in soup.find_all('a'):
+      if(not a['href'].startswith('http')):
+        a['href'] = '/' + a['href']
+
+    sessionAnchors = soup.find(id="Session_Notes").find_all('a')
+    for a in sessionAnchors:
+      # Make all session note links relative
+      a['href'] = a['href'][1:]
+
+    overwriteFile(indexFile, soup)
+
+updateIndexFile(sys.argv[1])
+# shutil.copyfile
