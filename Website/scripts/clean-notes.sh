6446a29f54104db8d7e560564ae8397222eb6d3f
Nathan Orick <cnathanorick@gmail.com>
2023-06-11 18:03:40 -0400

    vault backup: 2023-06-11 18:03:40

--- Website/scripts/clean-notes.sh
+++ Website/scripts/clean-notes.sh
@@ -5,7 +5,7 @@ rm -r no-spoilers/*
 mkdir no-spoilers/campaign-notes
 cp site/campaign-notes.html no-spoilers/campaign-notes/index.html
 cp site/new-handouts.html no-spoilers/campaign-notes/new-handouts.html
-cp -r site/styles site/scripts no-spoilers/campaign-notes
+cp -r site/lib site/scripts no-spoilers/campaign-notes
 cp -r site/session-notes no-spoilers/campaign-notes/session-notes
 python3 scripts/clean-notes.py no-spoilers/campaign-notes/index.html no-spoilers/campaign-notes/session-notes
 python3 scripts/re-root-urls.py no-spoilers/campaign-notes/new-handouts.html
