63962ce1e469484df08a50a64ecde74969959935
Nathan Orick <cnathanorick@gmail.com>
2023-06-11 20:44:41 -0400

    vault backup: 2023-06-11 20:44:41

--- Website/scripts/clean-notes.py
+++ Website/scripts/clean-notes.py
@@ -77,11 +77,13 @@ def updateAllFiles(folder):
 def importAndCleanLinkedFiles(soup, spoilersFolder, rootSiteFolder):
   for a in soup.find_all('a', class_='internal-link'):
     if alreadyFixedLinkClass in a.get('class', []):
+      print('link already fixed: {}'.format(newPath))
       break
 
     relativePath = os.path.join(*a['href'].split('/'))
     newPath = os.path.join(spoilersFolder, relativePath)
     if os.path.isfile(newPath):
+      print('file already exists: {}'.format(newPath))
       break
     
     origPath = os.path.join(rootSiteFolder, relativePath)
