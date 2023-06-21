04deabf1c11d255ee25da87f7d4c2e5612847f41
Nathan Orick <cnathanorick@gmail.com>
2023-06-21 15:51:08 -0400

    vault backup: 2023-06-21 15:51:08

--- Website/scripts/clean-notes.sh
+++ Website/scripts/clean-notes.sh
@@ -2,10 +2,10 @@
 # Cleans links from the session notes
 
 cp favicon.ico site
-cp styles site
+cp styles.css site
 rm -r no-spoilers/*
 mkdir no-spoilers/campaign-notes
 cp site/campaign-notes.html no-spoilers/campaign-notes/index.html
 cp site/new-handouts.html no-spoilers/campaign-notes/new-handouts.html
-cp -r site/lib site/scripts no-spoilers/campaign-notes
+cp -r site/lib no-spoilers/campaign-notes
 python3 scripts/clean-notes.py no-spoilers/campaign-notes/ site
